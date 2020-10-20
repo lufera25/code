3fd95e7d180855499afe770065c02775fbb20a86
lufera25 <72897519+lufera25@users.noreply.github.com>
2020-10-19 19:35:25 -0600

    pitagoras

---
+++ D1S2/numbe-strings.js
@@ -0,0 +1,13 @@
+let numberOne = 15;
+
+// a^2 + b^2 = c^2
+function pitagoras(a, b) {
+
+    c = Math.pow(a, 2) + Math.pow(b, 2) 
+
+   return   Math.sqrt(c)
+
+
+}
+
+console.log(pitagoras(3, 3) )
