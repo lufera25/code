d79460bb3aa0385f93e39e13a6ce503daf364592
lufera25 <72897519+lufera25@users.noreply.github.com>
2020-10-24 22:59:39 -0600

    Tarea Terminada

--- D1S2/D4-Tarea/ejercicios.js
+++ D1S2/D4-Tarea/ejercicios.js
@@ -4,7 +4,7 @@
 // en base aun argumento pasado en f(n), donde n es un numero natural > 0
 // además concatenear la frase "... Leer más"
 
-const { forEach } = require("lodash");
+// const { forEach } = require("lodash");
 
 
 // Ej.
@@ -22,7 +22,7 @@ function f(n) {
   return texNuevo + "... Leer más";
 }
 
-console.log("Ejercicio 1 :"+f(10))
+console.log("Ejercicio 1: " + f(10));
 
 // 2. vocales
 // dado una cadena de texto de n caracteres
@@ -37,14 +37,14 @@ console.log("Ejercicio 1 :"+f(10))
  */
 const input2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`;
 
-console.log("Ejercicio 2: ")
-console.log("Texto Normal: " + input2)
-console.log("Texto Modificado: " + cambiarUltimaVocal(input2,"t"))
+console.log("Ejercicio 2: ");
+console.log("Texto Normal: " + input2);
+console.log("Texto Modificado: " + cambiarUltimaVocal(input2, "t"));
 
 function cambiarUltimaVocal(texto, letra) {
 
   const input3 = texto.split(" ");
-  let remplace = ""
+  let remplace = "";
   let output = "";
 
   for (let i = 0; i < input3.length; i++) {
@@ -56,19 +56,19 @@ function cambiarUltimaVocal(texto, letra) {
       if (cont == 0) {
         if (element[caracter] == "a" || element[caracter] == "e" || element[caracter] == "i" || element[caracter] == "o" || element[caracter] == "u") {
 
-          remplace += element[caracter].replace(element[caracter], letra)
+          remplace += element[caracter].replace(element[caracter], letra);
 
-          cont = 1
+          cont = 1;
 
         } else {
-          remplace += element[caracter]
+          remplace += element[caracter];
         }
       } else {
-        remplace += element[caracter]
+        remplace += element[caracter];
       }
 
     }
-    remplace += " "
+    remplace += " ";
   }
 
   const array = remplace.split(" ");
@@ -80,75 +80,67 @@ function cambiarUltimaVocal(texto, letra) {
     for (let caracter = palabra.length - 1; caracter >= 0; caracter--) {
       output += palabra[caracter];
     }
-    output += " "
+    output += " ";
+    return output;
+
   }
-  return output
 
 }
 
 
 
+  // 3. Wolfram Alpha
+  // https://www.wolframalpha.com/
+  // Wolfram Alpha es un sitio que ofrece una herremienta matemática y pedagógica
+  // que ayuda a los estudiantes a resolver ecuaciones matemáticas paso a paso
+  // y de forma gráfica
+  // Ejercicio,
+  // Dada una ecuacion de primer grado del tipo, ax + b = c
+  // donde a, b y c son valores constantes y 'x' es un valor desconocido o variable
+  // escribir ua funcion que tome una ecuación de primer grado
+  // y resuelva imprimiendo paso a paso la resolución de la misma
+  // Ej.
+  //  f('3x+6=0')
+  //  (Paso 1) -> 3x = -6
+  //  (Paso 2) -> x = -6/3
+  //  (Paso 3) -> x = -2
+  // fin
+  // BONUS (AYUDA)
+  // para input = "3x+6=0"
+  // donde el patron a buscar (?<ax>[0-9]*x)(?<operador>[\+|-])(?<b>[0-9]+)(?<igual>=)(?<c>[0-9]+)$
+  // y
+  // ?<ax> -> nombre del grupo
+  // * -> busca de cero conicidencias a ilimitadas
+  // + -> busca de una conicidencia a ilimitadas
+  // [\+|-] -> busca el operador + o el -
+  // $ -> estrictamente debe finalizar con un valor
+
+  ecuacionLineal("3x+6=0");
+
+  function ecuacionLineal(ecuacion) {
+
+    console.log("Ejercicio3 ");
+    console.log("Ecuacion lineal: " + ecuacion);
+
+    const pattern = /(?<a>[0-9]+)(?<x>[\x])(?<operador>[\+|-])(?<b>[0-9]+)(?<igual>=)(?<c>[0-9]+)$/m;
+
+    const arrayResult = Array.from(ecuacion.match(pattern));
+
+    const paso1 = arrayResult[1] + arrayResult[2] + " = " + (arrayResult[6] > 0 ? arrayResult[6] : "") + (arrayResult[3] === "+" ? '-' + arrayResult[4] : '+' + arrayResult[4])
+
+    console.log("paso 1: " + paso1);
+
+    sumaDespeje = eval(arrayResult[6]) + eval(arrayResult[3] === "+" ? '-' + arrayResult[4] : arrayResult[4]);
+
+    const paso2 = arrayResult[2] + " = " + sumaDespeje + "/" + arrayResult[1];
+
+    console.log("paso 2: " + paso2);
+
+    divisionDespeje = sumaDespeje / eval(arrayResult[1]);
+
+    const paso3 = arrayResult[2] + " = " + divisionDespeje;
+
+    console.log("paso 3: " + paso3);
 
 
-// 3. Wolfram Alpha
-// https://www.wolframalpha.com/
-// Wolfram Alpha es un sitio que ofrece una herremienta matemática y pedagógica
-// que ayuda a los estudiantes a resolver ecuaciones matemáticas paso a paso
-// y de forma gráfica
-// Ejercicio,
-// Dada una ecuacion de primer grado del tipo, ax + b = c
-// donde a, b y c son valores constantes y 'x' es un valor desconocido o variable
-// escribir ua funcion que tome una ecuación de primer grado
-// y resuelva imprimiendo paso a paso la resolución de la misma
-// Ej.
-//  f('3x+6=0')
-//  (Paso 1) -> 3x = -6
-//  (Paso 2) -> x = -6/3
-//  (Paso 3) -> x = -2
-// fin
-// BONUS (AYUDA)
-// para input = "3x+6=0"
-// donde el patron a buscar (?<ax>[0-9]*x)(?<operador>[\+|-])(?<b>[0-9]+)(?<igual>=)(?<c>[0-9]+)$
-// y
-// ?<ax> -> nombre del grupo
-// * -> busca de cero conicidencias a ilimitadas
-// + -> busca de una conicidencia a ilimitadas
-// [\+|-] -> busca el operador + o el -
-// $ -> estrictamente debe finalizar con un valor
-
-ecuacionLineal("3x+6=0")
-
-function ecuacionLineal(ecuacion) {
-
-  console.log("Ejercicio3 ")
-  console.log("Ecuacion lineal: "+ ecuacion)
-
-  const pattern = /(?<a>[0-9]+)(?<x>[\x])(?<operador>[\+|-])(?<b>[0-9]+)(?<igual>=)(?<c>[0-9]+)$/m;
-  // const string = "3x+6=0";
-  const arrayResult = Array.from(ecuacion.match(pattern));
-
-  const paso1 = arrayResult[1] + arrayResult[2] + " = " + (arrayResult[6] > 0 ? arrayResult[6] : "") + (arrayResult[3] === "+" ? '-' + arrayResult[4] :'+' + arrayResult[4])
-  console.log("paso 1: " + paso1);
-
-  sumaDespeje = eval(arrayResult[6]) + eval(arrayResult[3] === "+" ? '-' + arrayResult[4] : arrayResult[4]);
-
-  const paso2 = arrayResult[2] + " = " + sumaDespeje + "/" + arrayResult[1];
-
-  console.log("paso 2: " + paso2);
-
-  divisionDespeje = sumaDespeje / eval(arrayResult[1])
-
-  const paso3 = arrayResult[2] + " = " + divisionDespeje
-
-  console.log("paso 3: " + paso3)
-
-
-}
-
-
-
-  // console.log(arrayResult)
-
-  // arrayResult = [ '3x+6=0', '3x', '+', '6', '=', '0' ]
-
-
+  }
